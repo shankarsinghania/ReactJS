b394a435194614f5fde0f4d95853c2f9df0cc52a
Shankar Kumar <shankar@website.com>
2025-08-30 12:08:48 +0530

    helloworld

---
+++ shankar-vite/src/App.jsx
@@ -0,0 +1,14 @@
+import Counter from "./components/Counter"
+
+function App() {
+  return (
+   <>
+   <div>App</div>
+   <Counter initialValue = {100} />
+   <Counter initialValue = {200} />
+   <Counter initialValue = {300} />
+   </>
+  )
+}
+
+export default App
---
+++ shankar-vite/src/components/Counter.jsx
@@ -0,0 +1,11 @@
+ function Counter(props){
+    return (
+        <>
+            <button>-</button>
+            {props.initialValue}
+            <button>+</button>
+        </>
+    )
+ }
+
+ export default Counter;
